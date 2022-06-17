a417362395e2f3df23001dbfc837b3608a21008f
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-06-17 17:36:01 +0900

    [Platinum IV] Title: 도시 왕복하기 1, Time: 12 ms, Memory: 3284 KB -BaekjoonHub

---
+++ 백준/Platinum/17412. 도시 왕복하기 1/도시 왕복하기 1.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+const int INF = 987654321;
+const int START = 1;
+const int DEST= 2;
+
+
+int v, e;
+int cap[401][401];
+int flow[401][401];
+int parent[401];
+
+void add_edge(int from, int to, int c)
+{
+    cap[from][to] += c;
+}
+int cal_amt() // this case, just one..but practice!
+{
+    int amt = INF;
+    int here = DEST;
+    while (parent[here] != here)
+    {
+        amt = min(amt, cap[parent[here]][here] - flow[parent[here]][here]);
+        here = parent[here];
+    }
+
+    return amt;
+}
+bool bfs() // true: success to get DIST, or false
+{
+    memset(parent, 0, sizeof(parent));
+    queue<int> q;
+    q.push(START);
+    parent[START] = START;
+
+    int amt = INF;
+    while (!q.empty())
+    {
+        int here = q.front(); q.pop();
+
+        for (int next = 1; next <= v; ++next) {
+            if (parent[next] != 0 || cap[here][next] - flow[here][next] <= 0)
+                continue;
+
+            q.push(next);
+            parent[next] = here;
+        }
+       
+    }
+
+    // fail to get dest
+    if (parent[DEST] == 0)
+        return false;
+    else
+        return true;
+}
+void send(int amt) 
+{
+    int here = DEST;
+    while (parent[here] != here)
+    {
+        flow[parent[here]][here] += amt;
+        flow[here][parent[here]] -= amt;
+        here = parent[here];
+    }
+}
+
+
+int main()
+{
+    cin.tie(nullptr); cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    cin >> v >> e;
+    for (int i = 0; i < e; ++i) {
+        int from, to;
+        cin >> from >> to;
+        add_edge(from, to, 1);
+    }
+
+
+    int ans = 0;
+    while ( bfs() )
+    {
+        // cal max possible amount
+        int max_amount = cal_amt();
+        send(max_amount);
+        ans += max_amount;
+    }
+
+    cout << ans << endl;
+}
