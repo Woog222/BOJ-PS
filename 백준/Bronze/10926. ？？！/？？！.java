36849747f4ab5a0bdf758b572ebf796a1e1299a8
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-06-19 18:45:37 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        bw.write(s + "??!");
+        bw.newLine();
+        
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
