9aef0e9066371b43004986997be54092c4441e74
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-06-19 17:41:17 +0900

    [Silver III] Title: N과 M (3), Time: 452 ms, Memory: 68040 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    private static int INF = 1987654321;
+    private static int[] arr = new int[10];
+    private static boolean[] use = new boolean[10];
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        fun(0,0);
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static private void fun(int idx, int before) {
+        if (idx==m) {
+            // print
+            for (int i=0; i<m ;++i) {
+                sb.append(arr[i]);
+                sb.append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i=1; i<=n; ++i) {
+            //if (use[i]) continue;
+
+            //use[i] = true;
+            arr[idx] = i;
+            fun(idx+1, i);
+            //use[i] = false;
+        }
+    }
+
+}
