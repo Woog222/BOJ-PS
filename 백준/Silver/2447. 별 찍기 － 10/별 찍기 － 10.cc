ba1230f493a4a8e09ee7f22b405e4684838e5588
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-06-26 23:38:27 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 64 ms, Memory: 44072 KB -BaekjoonHub

--- 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -2,47 +2,65 @@
 #include <vector>
 #include <algorithm>
 #include <tuple>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <assert.h>
+#include <string>
+#include <map>
+#include <set>
+#include <iterator>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = 987654321;
 
-int n;
-void star(vector<vector<char>> &a, int r, int c, int size, bool blank)
-{
-	if (size==1)
-	{
-		if (blank) {
-			a[r][c] = ' '; return;
-		}
-		else{
-			a[r][c] = '*'; return;
-		}
-	}
-	
-	int nsize = size/3;
-	if(blank) {
-		for (int i=r; i<r+size; ++i) {
-			for (int j=c; j<c+size; ++j) {
-				a[i][j] = ' ';
-			}
-		}
-	}
-	else {
-		for (int i=0; i<3; ++i) {
-			for (int j=0; j<3; ++j) {
-				if (i==1 && j==1)
-					star(a, r+i*nsize, c+j*nsize, nsize, true);
-				else
-					star(a, r+i*nsize, c+j*nsize, nsize, false);
-			}
-		}
-	}
-}
+
+int dr[] = { 0,0,0,1,1,2,2,2 };
+int dc[] = { 0,1,2,0,2,0,1,2 };
+char grid[6562][6562];
+void fun(int r, int c, int size);
+
 int main()
 {
-	cin >> n;
-	vector<vector<char>> a(n, vector<char>(n,0));
-	star(a, 0,0,n, false);
-	for (int i = 0; i<n; ++i)
-	{
-		cout << string(a[i].begin(), a[i].end()) << '\n';
-	}
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            grid[i][j] = ' ';
+        }
+        grid[i][n] = '\0';
+    }    
+    fun(0, 0, n);
+
+    for (int i = 0; i < n; ++i) {
+        printf("%s\n", grid[i]);
+    }
+   
+}
+
+void fun(int r, int c, int size)
+{
+    if (size == 1) {
+        grid[r][c] = '*';
+        return;
+    }
+
+    int nsize = size / 3;
+    for (int k = 0; k < 8; ++k) {
+        int nr = r + dr[k] * nsize;
+        int nc = c + dc[k] * nsize;
+        fun(nr, nc, nsize);
+    }
 }
