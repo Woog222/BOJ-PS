a948a8cec5f3f16661d264d7b270ff24688c7745
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-07-22 23:51:53 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <assert.h>
+#include <string>
+#include <map>
+#include <set>
+#include <iterator>
+#include <cstdlib>
+#include <climits>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = 987654321;
+const int MOD = 10007;
+
+
+int main()
+{   
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    int a, b, v; cin >> a >> b >> v;
+    int day = a - b;
+
+    cout << (v - a + day - 1) / day + 1 << endl;
+}
