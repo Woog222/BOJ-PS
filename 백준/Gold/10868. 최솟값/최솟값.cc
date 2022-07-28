e067b718975db4a54e967602d8e96d0fb75bd64b
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-07-28 09:14:40 +0900

    [Gold I] Title: 최솟값, Time: 64 ms, Memory: 9052 KB -BaekjoonHub

--- 백준/Gold/10868. 최솟값/최솟값.cc
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -5,106 +5,56 @@
 #include <queue>
 #include <cstring>
 #include <cmath>
-#include <iomanip>
 #include <list>
 #include <stack>
 #include <assert.h>
 #include <string>
 #include <map>
 #include <set>
-#include <iterator>
 #include <cstdlib>
-#include <ctime>
+#include <climits>
+#include <cstdio>
+#define el '\n'
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
+typedef vector <ll> vi;
 typedef vector<vi> vvi;
 typedef pair<int, int> pii;
-const int INF = 1987654321;
-const int MOD = 20090711;
+const int INF = numeric_limits<int>::max();
+const int MOD = 1000000007;
 
 
-struct RMQ {
+int N;
+int arr[100000];
+int dp[100000][17];
 
-    int n;
-
-    vi rangeMin;
-    RMQ(const vector<int>& array) {
-        n = array.size();
-        rangeMin.resize(4 * n);
-        init(array, 0, n - 1, 1);
-    }
-
-    int query(int lo, int hi) {
-        return query(lo, hi, 1, 0, n - 1);
-    }
-
-    void update(int idx, int nval) {
-        update(idx, nval, 1, 0, n - 1);
-    }
-
-private:
-    int init(const vi& array, int lo, int hi, int idx) 
-    {
-        if (lo == hi)
-            return rangeMin[idx] = array[lo];
-
-        int mid = (lo + hi) / 2;
-        int leftMin = init(array, lo, mid, idx * 2);
-        int rightMin = init(array, mid + 1, hi, idx * 2 + 1);
-
-        return rangeMin[idx] = min(leftMin, rightMin);
-    }
-
-    int query(int lo, int hi, int node, int nodeLeft, int nodeRight)
-    {
-        if (lo <= nodeLeft && nodeRight <= hi)
-            return rangeMin[node];
-
-        int mid = (nodeLeft + nodeRight) / 2;
-        int ret = INF;
-
-        if (lo <= mid) {
-            ret = min(ret, query(lo, hi, node * 2, nodeLeft, mid) );
+int query(int i, int j) {
+    int ret = arr[i], k = 17;
+    while (i <= j && k >= 0) {
+        if (i + (1 << k) - 1 <= j) {
+            ret = min(ret, dp[i][k]);
+            i += 1 << k;
         }
-        if (mid < hi) {
-            ret = min(ret, query(lo, hi, node * 2 + 1, mid + 1, nodeRight));
-        }
-        return ret;
+        k--;
     }
-
-    int update(int idx, int nval, int node, int nodeLeft, int nodeRight) 
-    {
-        if (nodeLeft == nodeRight)
-            return rangeMin[node] = nval;
-
-        int mid = (nodeLeft + nodeRight) / 2;
-        if (idx <= mid)
-            rangeMin[node] = min(rangeMin[node], 
-                                update(idx, nval, node * 2, nodeLeft, mid));
-        else
-            rangeMin[node] = min(rangeMin[node],
-                update(idx, nval, node * 2+1, mid+1, nodeRight));
-
-        return rangeMin[node];
-    }
-};
-
-
+    return ret;
+}
 
 int main()
 {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    int n, m; cin >> n >> m;
-    vi arr(n);
-    for (int i = 0; i < n; ++i)
-        cin >> arr[i];
-
-    RMQ seg = RMQ(arr);
+    cin.tie(nullptr); ios_base::sync_with_stdio(false); cout.tie(nullptr);
+    int m; cin >> N >> m;
+    for (int i = 0; i < N; ++i) cin >> arr[i];
+    for (int i = 0; i < N; ++i) dp[i][0] = arr[i];
+    for (int j = 1; j < 17; ++j) {
+        for (int i = 0; i < N; ++i) {
+            if (i + (1 << j) > N) break;
+            dp[i][j] = min(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+        }
+    }
 
     while (m--) {
-        int lo, hi; cin >> lo >> hi;
-        cout << seg.query(lo - 1, hi - 1) << '\n';
+        int i, j; cin >> i >> j;
+        cout << query(i-1, j-1) << el;
     }
 }
