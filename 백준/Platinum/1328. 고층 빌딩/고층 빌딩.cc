52014415837d2fbf1c1a21774567ad23c5a1bcce
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-08-01 10:19:54 +0900

    [Platinum V] Title: 고층 빌딩, Time: 4 ms, Memory: 10068 KB -BaekjoonHub

--- 백준/Platinum/1328. 고층 빌딩/고층 빌딩.cc
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.cc
@@ -2,43 +2,52 @@
 #include <vector>
 #include <algorithm>
 #include <tuple>
-#include <set>
 #include <queue>
 #include <cstring>
 #include <cmath>
+#include <list>
+#include <stack>
+#include <assert.h>
+#include <string>
 #include <map>
+#include <set>
+#include <cstdlib>
+#include <climits>
+#include <cstdio>
+#define el '\n'
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef vector<vvi> vvvi;
 typedef pair<int, int> pii;
-const int INF = -987654321;
-const ll MOD = 1000000007;
+const int INF = 987654321;
+const int MOD = 1000000007;
 
-ll cache[101][101][101];
+ll dp[101][101][101];
 
-
-
-
-
-int main() 
-{
-    cin.tie(nullptr); ios_base::sync_with_stdio(false);
-    int n, l, r; cin >> n >> l >> r;
-
-    memset(cache, 0, sizeof(cache));
-    
-    cache[1][1][1] = 1LL;
-    for (int N = 2; N <= n; ++N) {
-        for (int L = 1; L <= n; ++L) {
-            for (int R = 1; R <= n; ++R) {
-                cache[N][L][R] = (cache[N - 1][L - 1][R] + cache[N - 1][L][R - 1]
-                    + (N - 2) * cache[N - 1][L][R] ) % MOD;
-            }
-        }
+ll fun(int n, int l, int r) {
+    if (l + r - 1 > n) return 0;
+    if (l <= 0 || r <= 0) return 0;
+    if (n == 1) {
+        if (l == 1 && r == 1) return 1;
+        else return 0;
     }
 
-    cout << cache[n][l][r] << endl;
+
+    ll& ret = dp[n][l][r];
+    if (ret != -1) return ret;
+
+    ret = 0;
+    ret += fun(n - 1, l - 1, r); ret %= MOD;
+    ret += fun(n - 1, l, r - 1); ret %= MOD;
+    ret += fun(n - 1, l, r) * (n - 2); ret %= MOD;
+    return ret;
 }
 
+int main()
+{
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    memset(dp, -1, sizeof(dp));
+    int n, l, r; cin >> n >> l >> r;
+    cout << fun(n, l, r) << el;
+}
