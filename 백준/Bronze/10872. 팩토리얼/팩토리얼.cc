991b4a69127ebd1342dd71be91a18caa9323b6bd
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-08-02 14:13:07 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <list>
+#include <stack>
+#include <assert.h>
+#include <string>
+#include <map>
+#include <set>
+#include <cstdlib>
+#include <climits>
+#include <cstdio>
+#define el '\n'
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = 987654321;
+const int MOD = 1000000007;
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    else return n * factorial(n - 1);
+}
+
+int main() {
+    int n; cin >> n;
+    cout << factorial(n) << el;
+}
