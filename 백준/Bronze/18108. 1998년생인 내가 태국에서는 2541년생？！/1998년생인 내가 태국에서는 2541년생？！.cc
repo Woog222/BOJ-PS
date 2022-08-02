53c3b2ab8a375b9e49f647c12f88194bc39e9977
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-08-02 13:18:03 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <list>
+#include <stack>
+#include <assert.h>
+#include <string>
+#include <map>
+#include <set>
+#include <cstdlib>
+#include <climits>
+#include <cstdio>
+#define el '\n'
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = 987654321;
+const int MOD = 1000000007;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+    cout << n - (2541 - 1998) << el;
+}
