3b1eb4152d31daa247bee05a4a9cf33e2bea55fe
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-08-04 08:56:43 +0900

    [Platinum IV] Title: 오일러 회로, Time: 760 ms, Memory: 345124 KB -BaekjoonHub

--- 백준/Platinum/1199. 오일러 회로/오일러 회로.cc
+++ 백준/Platinum/1199. 오일러 회로/오일러 회로.cc
@@ -21,41 +21,69 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef pair<int, int> pii;
 const ll INF = numeric_limits<ll>::min();
+const int N = 1000;
 
+struct Edge {
+    int u, v, cnt;
+};
 
-int start[1000], adj[1000][1000]; // i-j 간선의 수
-int N;
+vector<int> adj[N];
+vector<Edge> edge;
 deque<int> circuit;
 
 void dfsEuler(int here) {
-    for (int there = start[here]; there < N; ++there) {
-        start[here] = there;
-        while (adj[here][there] > 0) {
-            adj[here][there]--;
-            // 방향그래프인 경우 아래코드 사용하지 않음
-            adj[there][here]--;
-            dfsEuler(there);
+
+    while (!adj[here].empty()) {
+        int idx = adj[here].back();
+        auto& e = edge[idx];
+        assert(&e == &edge[idx]);
+        if (e.cnt > 0) {
+            e.cnt--;
+            dfsEuler((e.u == here) ? e.v : e.u);
         }
+        else
+            adj[here].pop_back();
     }
     circuit.push_front(here);
 }
+
+bool readInput() {
+    int n; cin >> n;
+    vvi temp(n, vi(n));
+
+    bool ok = true;
+    for (int i = 0; i < n; ++i) {
+        int deg = 0;
+        for (int j = 0; j < n; ++j) {
+            cin >> temp[i][j];
+            deg += temp[i][j];
+        }
+        if (deg % 2) ok = false;
+    }
+    if (!ok) return false;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i+1; j < n; ++j) {
+            int cnt = temp[i][j];
+            if (cnt > 0) {
+                edge.push_back(Edge{ i,j,cnt });
+                int idx = (int)edge.size() - 1;
+                adj[i].push_back(idx);
+                adj[j].push_back(idx);
+            }
+        }
+    }
+    return true;
+}
+
 int main() {
     cin.tie(nullptr); ios_base::sync_with_stdio(false);
-    cin >> N;
-    int e = 0;
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j) {
-            cin >> adj[i][j];
-            e += adj[i][j];
-        }
-    e /= 2;
+    if (!readInput()) {
+        cout << -1 << el;
+        return 0;
+    }
 
     dfsEuler(0);
-    if (circuit.front() != circuit.back() || circuit.size() != e+1 ) {
-        cout << -1 << el;
-    }
-    else {
-        for (int v : circuit) cout << v+1 << " ";
-        cout << el;
-    }
+    for (int i : circuit) cout << i+1 << " ";
+    cout << el;
 }
