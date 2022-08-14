ee7ac04a63733fb42d8c1e91a2d44c822929ca14
Woog222 <96822822+Woog222@users.noreply.github.com>
2022-08-14 11:13:16 +0900

    [Platinum III] Title: 2-SAT - 4, Time: 32 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Platinum/11281. 2－SAT － 4/2－SAT － 4.cc
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <list>
+#include <stack>
+#include <assert.h>
+#include <string>
+#include <map>
+#include <set>
+#include <cstdlib>
+#include <climits>
+#include <cstdio>
+#define el '\n'
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = 987654321;
+const int MAX = 20005;
+
+
+int N, M;
+vi adj[MAX]; // (2,3) ~~ (2n, 2n+1) (+, -)
+stack<int> st;
+int sccId[MAX], order[MAX]; // memset to -1
+int vcnt, scccnt;
+
+int dfsScc(int here) {
+    int ret = order[here] = vcnt++;
+    st.push(here);
+
+    for (int there : adj[here]) {
+        if (order[there] == -1) 
+            ret = min(ret, dfsScc(there));
+        else if (sccId[there] == -1) 
+            ret = min(ret, order[there]);
+    }
+
+    if (ret == order[here]) {
+        while (true) {
+            int temp = st.top(); st.pop();
+            sccId[temp] = scccnt;
+            if (temp == here) break;
+        }
+        scccnt++;
+    }
+    return ret;
+}
+
+vi fun() {
+    vi ans(N + 1, -1);
+    vector<pii> temp;
+
+    for (int i = 2; i <= 2 * N + 1; ++i) 
+        temp.emplace_back(-sccId[i], i);
+    sort(temp.begin(), temp.end());
+
+    for (pii& p : temp) {
+        int v = p.second / 2, isTrue = p.second % 2 == 0;
+        if (ans[v] != -1) continue;
+        ans[v] = 1-isTrue;
+    }
+    return ans;
+
+}
+void readInput();
+int main() {
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    readInput();
+    memset(sccId, -1, sizeof(sccId));
+    memset(order, -1, sizeof(order));
+    for (int i = 2; i <= 2 * N + 1; ++i) if (order[i] == -1) dfsScc(i);
+    // Contradiction Check
+    for (int i = 1; i <= N; ++i) {
+        if (sccId[i * 2] == sccId[i * 2+1]) {
+            cout << 0 << el;
+            return 0;
+        }
+    }
+
+    auto ans = fun();
+    cout << 1 << el;
+    for (int i = 1; i <= N; ++i) cout << ans[i] << " ";
+    cout << el;
+
+    
+}
+
+void readInput() {
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i) {
+        int a, b; cin >> a >> b;
+        a = (a < 0) ? (-a) * 2 + 1 : 2 * a;
+        b = (b < 0) ? (-b) * 2 + 1 : 2 * b;
+
+        adj[(a % 2) ? a - 1 : a + 1].push_back(b);
+        adj[(b % 2) ? b - 1 : b + 1].push_back(a);
+    }
+}
